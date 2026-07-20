f1a8839fdf090b080923e58db2d322c4756391a5
operavs23 <arthosowdagor@gmail.com>
2026-07-20 13:58:04 +0600

    Create Student class declaration

---
+++ include/Student.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+class Student {
+    private:
+        int studentId;
+        std::string studentName;
+        std::string department;
+        double cgpa;
+        std::string studentPhone;
+        std::string studentEmail;
+    public:
+
+};
